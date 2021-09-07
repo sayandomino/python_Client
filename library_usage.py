ceef0564909184f73531a8fe80c44ac8eb9e6de7
sayandomino <sayannayak@gmail.com>
2021-09-07 15:41:48 +0530

    Checkstyle updated

--- library_usage.py
+++ library_usage.py
@@ -5,24 +5,24 @@ from Client.prediction_client import PredictionClient
 
 pred_client = PredictionClient()
 
-feature_names = ['dropperc', 'mins', 'consecmonths', 'income', 'age']
-feature_values = ['dropperc', 'mins', 'consecmonths', 'income', 'age']
+feature_names = ["dropperc", "mins", "consecmonths", "income", "age"]
+feature_values = ["dropperc", "mins", "consecmonths", "income", "age"]
 
 features_dict = dict(zip(feature_names, feature_values))
 
-predict_names = ['y']
+predict_names = ["y"]
 predict_values = [100]
 
 predict_dict = dict(zip(predict_names, predict_values))
 
 # Record eventID and current time
 event_id = uuid.uuid4()
-event_time = datetime.datetime.now().timestamp()*1000
+event_time = datetime.datetime.now().timestamp() * 1000
 
 # Custom metadata I want to track for this event
-metadata = {'cohort': 'cohort_id'}
-metadata_names = ['cohort']
-metadata_values = ['cohort_id']
+metadata = {"cohort": "cohort_id"}
+metadata_names = ["cohort"]
+metadata_values = ["cohort_id"]
 
 pred_client.init_models(feature_names, predict_names, metadata_names)
 
--- setup.py
+++ setup.py
@@ -1,20 +1,18 @@
 from setuptools import setup
 
-PACKAGE_NAME = 'prediction-client'
+PACKAGE_NAME = "prediction-client"
 
 setup(
-    name='python_client',
-    version='1.0.0',
-    author='Domino Data Lab',
-    author_email='support@dominodatalab.com',
+    name="python_client",
+    version="1.0.0",
+    author="Domino Data Lab",
+    author_email="support@dominodatalab.com",
     packages=[PACKAGE_NAME],
     scripts=[],
-    url='http://www.dominodatalab.com',
-    license='LICENSE.txt',
-    description='Python Client',
-    long_description='',
-    install_requires=[
-    ],
-    extras_require={
-    }
+    url="http://www.dominodatalab.com",
+    license="LICENSE.txt",
+    description="Python Client",
+    long_description="",
+    install_requires=[],
+    extras_require={},
 )
